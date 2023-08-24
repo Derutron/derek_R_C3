0a81a6b1803fb8e241c6fbdac81cdf3c2be46d91
Derutron <fidg_1@yahoo.com>
2023-08-24 21:18:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ function App() {
   return (
     <div className="App">
       <h1>Derek Fidoro</h1>
-      <p>Educational Background: Bachelor's in Electrical Electronics Engineering</p>
-      <p>Professional Background: Software Engineer, Intern at FivSys</p>
-      <p>Skills: JavaScript, React, HTML, CSS</p>
+      <h2>Educational Background</h2>
+      <p>Bachelor of Science in Electrical Electronics Engineering</p>
+      <p>Anambra State University</p>
+      
+      <h2>Professional Background</h2>
+      <p>Control Room Operator at Geometric Power</p>
+      <p>Intern, Software Developer at FivSys</p>
+      
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>HTML/CSS</li>
+        <li>Node.js</li>
+      </ul>
     </div>
   );
 }
