dcad02e38a302e36b4be137aa5163f1d8dc30d90
Derutron <fidg_1@yahoo.com>
2023-08-24 21:08:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>John Doe</h1>
-      <p>Educational Background: Bachelor's in Computer Science</p>
-      <p>Professional Background: Software Engineer at ABC Company</p>
+      <h1>Derek Fidoro</h1>
+      <p>Educational Background: Bachelor's in Electrical Electronics Engineering</p>
+      <p>Professional Background: Software Engineer, Intern at FivSys</p>
       <p>Skills: JavaScript, React, HTML, CSS</p>
     </div>
   );
